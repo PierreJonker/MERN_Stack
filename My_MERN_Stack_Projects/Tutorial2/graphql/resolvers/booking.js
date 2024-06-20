9ce44376471f9a21da43c513a7c15b06a6906251
Pierre <jonkerpierre637@gmail.com>
2024-06-20 15:31:23 +0200

    Video 9 Done

---
+++ My_MERN_Stack_Projects/Tutorial2/graphql/resolvers/booking.js
@@ -0,0 +1,35 @@
+const Event = require('../../models/event.js');
+const Booking = require('../../models/booking.js');
+const { transformBooking, transformEvent } = require('./merge.js');
+
+module.exports = {
+  bookings: async () => {
+    try {
+      const bookings = await Booking.find();
+      return bookings.map(booking => {
+        return transformBooking(booking) ;
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  bookEvent: async args => {
+    const fetchedEvent = await Event.findOne({ _id: args.eventId });
+    const booking = new Booking({
+      user: '66729cf0263ff64f6498fc53',
+      event: fetchedEvent
+    });
+    const result = await booking.save();
+    return transformBooking(result);
+  },
+  cancelBooking: async args => {
+    try {
+      const booking = await Booking.findById(args.bookingId).populate('event');
+      const event = transformEvent(booking.event);
+      await Booking.deleteOne({ _id: args.bookingId });
+      return event;
+    } catch (err) {
+      throw err;
+    }
+  }
+};
